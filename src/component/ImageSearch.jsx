88f0f8755ccdb6d2ab8f74021283a8ff260e2dd5
Alya Rusyda Maharistya <100183012+AlyaRusyda@users.noreply.github.com>
2023-02-04 19:27:12 +0700

    Update and rename Search.js to ImageSearch.jsx

--- src/component/ImageSearch.jsx
+++ src/component/ImageSearch.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Search = ({ searchText }) => {
+const ImageSearch = ({ searchText }) => {
   const [text, setText] = useState('');
 
   const onSubmit = (e) => {
@@ -9,10 +9,10 @@ const Search = ({ searchText }) => {
   }
 
   return (
-    <div className='max-w-sm rounded overflow-hidden my-10 mx-auto mt-4'>
-      <form onSubmit={onSubmit} className="w-full max-w-sm bg-white/60 p-1">
+    <div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+      <form onSubmit={onSubmit} className="w-full max-w-sm">
         <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
-        <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image..." />
+        <input onChange={e => setText(e.target.value)} className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" type="text" placeholder="Search Image Term..." />
         <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
       Search
       </button>
@@ -22,4 +22,4 @@ const Search = ({ searchText }) => {
   )
 }
 
-export default Search;
+export default ImageSearch;
